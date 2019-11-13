d0abb7f78e54fe7c350a42e8be34a799ad00000d
skutner <sava.dumitru.daniel@gmail.com>
2019-11-13 14:06:35 +0200

    removed asset FileAnchor

--- defaultConstitution/assets/index.js
+++ defaultConstitution/assets/index.js
@@ -5,7 +5,6 @@ module.exports = $$.library(function(){
     require("./ACLScope");
     require("./Key");
     require("../transactions/transactions");
-    require("./FileAnchor");
     require("./BarAnchor");
     require('./CSBMeta');
 });
