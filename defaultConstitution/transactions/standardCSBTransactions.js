4b96265a76ed925a7d02498cfacf860c3efcaa7c
skutner <sava.dumitru.daniel@gmail.com>
2019-11-21 16:33:11 +0200

    added addFileAnchor transaction

--- defaultConstitution/transactions/standardCSBTransactions.js
+++ defaultConstitution/transactions/standardCSBTransactions.js
@@ -2,5 +2,10 @@ $$.transaction.describe("StandardCSBTransactions", {
     addBarAnchor: function (mountPoint, barMapDigest) {
         this.transaction.createAsset("BarAnchor", "init", mountPoint, barMapDigest);
         this.commit();
+    },
+
+    addFileAnchor: function (digest,) {
+        this.transaction.createAsset("FileAnchor", "init", digest);
+        this.commit();
     }
 });
