9a1e00c7a8b3bd139559fb2a4937d1d62513410e
skutner <sava.dumitru.daniel@gmail.com>
2020-01-14 15:54:16 +0200

    implemented BarCache

--- strategies/worldStateCaches/worldStateCacheRegistry.js
+++ strategies/worldStateCaches/worldStateCacheRegistry.js
@@ -37,6 +37,67 @@ function StorageContainer(){
     }
 }
 
+function BarCache(archive) {
+    let storage = new StorageContainer();
+    this.readKey = storage.readKey;
+    this.writeKey = storage.writeKey;
+    this.updateAliases = storage.updateAliases;
+
+    //just in case the folder got to use as storage does not exist
+
+    const worldStateCachePath = "/worldStateCache";
+
+    this.getState = function (callback) {
+        archive.readFile(worldStateCachePath,  function (err, res) {
+            let objRes = {};
+            if (err) {
+                callback(err, objRes);
+                console.log("Initialisating empty blockchain state");
+            } else {
+                objRes = JSON.parse(res);
+                storage.pskdb = objRes.pskdb;
+                storage.keys  = objRes.keys;
+                storage.pulse  = objRes.pulse;
+                callback(null, storage.pskdb);
+            }
+        });
+    };
+
+    this.updateState = function (internalValues, callback) {
+        storage.pskdb = internalValues;
+        archive.writeFile(worldStateCachePath, JSON.stringify(storage, null, 1), callback);
+    };
+
+    this.dump = function(){
+        console.log("EDFSCache:", storage);
+    }
+
+}
+
+function MemoryCache() {
+    let storage = new StorageContainer();
+    this.readKey = storage.readKey;
+    this.writeKey = storage.writeKey;
+    this.updateAliases = storage.updateAliases;
+
+    this.getState = function (callback) { //err, valuesFromCache
+        callback(null, storage.pskdb);
+    };
+
+    this.updateState = function (internalValues, callback) {
+        //console.info("Commiting state in memory cache "/*, internalValues*/)
+        storage.pskdb = internalValues;
+        storage.pulse = internalValues.pulse;
+        setImmediate(() => {
+            callback(null, storage.pskdb);
+        });
+    };
+
+    this.dump = function(){
+        console.log("MemoryCache:", storage);
+    }
+}
+
 function LocalWSCache(folder) {
     let storage = new StorageContainer();
     this.readKey = storage.readKey;
@@ -76,36 +137,14 @@ function LocalWSCache(folder) {
 
 }
 
-function MemoryCache() {
-    let storage = new StorageContainer();
-    this.readKey = storage.readKey;
-    this.writeKey = storage.writeKey;
-    this.updateAliases = storage.updateAliases;
-
-    this.getState = function (callback) { //err, valuesFromCache
-        callback(null, storage.pskdb);
-    };
-
-    this.updateState = function (internalValues, callback) {
-        //console.info("Commiting state in memory cache "/*, internalValues*/)
-        storage.pskdb = internalValues;
-        storage.pulse = internalValues.pulse;
-        setImmediate(() => {
-            callback(null, storage.pskdb);
-        });
-    };
-
-    this.dump = function(){
-        console.log("MemoryCache:", storage);
-    }
-}
 
 module.exports = {
     createCache: function (cacheType, ...args) {
         switch (cacheType) {
             case "fs":
                 return new LocalWSCache(...args);
-            case "none":
+            case "bar":
+                return new BarCache(...args);
             case "memory":
                 return new MemoryCache(...args);
             default:
