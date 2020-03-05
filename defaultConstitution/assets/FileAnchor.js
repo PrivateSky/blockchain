17643d8ec6a3d7857ab231bbccdd7bfb635afce6
skutner <sava.dumitru.daniel@gmail.com>
2020-03-05 09:48:55 +0200

    added SEED to FileAnchor

--- defaultConstitution/assets/FileAnchor.js
+++ defaultConstitution/assets/FileAnchor.js
@@ -2,13 +2,15 @@ $$.asset.describe("FileAnchor", {
     public: {
         alias: "string",
         type: "string",
+        seed: "string",
         digest: "string", //csb digest after file addition
         readList: "array", //encrypted seeds with public keys
         writeList: "array", //agentIds
     },
-    init: function (alias, type, digest) {
+    init: function (alias, type, seed, digest) {
         this.alias = alias;
         this.type = type;
+        this.seed = seed;
         this.digest = digest;
     }
 });
