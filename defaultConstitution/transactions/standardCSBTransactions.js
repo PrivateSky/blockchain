19647e731934a1544f5942a7c6b53dbec22d268c
skutner <sava.dumitru.daniel@gmail.com>
2020-03-05 11:30:48 +0200

    added phase getSeed in StandardCSBTransactions

--- defaultConstitution/transactions/standardCSBTransactions.js
+++ defaultConstitution/transactions/standardCSBTransactions.js
@@ -4,9 +4,9 @@ $$.transaction.describe("StandardCSBTransactions", {
         this.commit();
     },
 
-    addFileAnchor: function (alias, type, digest) {
+    addFileAnchor: function (alias, type, seed, digest) {
         try{
-            let fileAnchor = this.transaction.createAsset("FileAnchor", "init", alias, type, digest);
+            let fileAnchor = this.transaction.createAsset("FileAnchor", "init", alias, type, seed, digest);
 
             this.onCommit(() => {
                 this.return(undefined, fileAnchor.asJSON());
@@ -27,11 +27,18 @@ $$.transaction.describe("StandardCSBTransactions", {
             this.return(err.message);
         }
     },
-
     updateFileDigest: function (alias, digest) {
         const file = this.transaction.lookup("FileAnchor", alias);
         file.digest = digest;
         this.transaction.add(file);
         this.transaction.commit();
+    },
+    getSeed: function (alias) {
+        try {
+            const anchor = this.transaction.lookup("FileAnchor", alias);
+            this.return(undefined, anchor.seed);
+        } catch (e) {
+            this.return(e.message);
+        }
     }
 });
